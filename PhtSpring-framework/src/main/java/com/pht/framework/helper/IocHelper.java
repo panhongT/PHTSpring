fa6d3f3036e8895de7f3c2e3c687d845e8a1d050
Hamtin <panht999@163.com>
2022-10-27 15:23:13 +0800

    实现依赖注入——IocHelper类——实现简单的依赖注入功能。

--- PhtSpring-framework/src/main/java/com/pht/framework/helper/IocHelper.java
+++ PhtSpring-framework/src/main/java/com/pht/framework/helper/IocHelper.java
@@ -11,7 +11,7 @@ import com.pht.framework.util.ReflectionUtil;
 /**
  * 依赖注入助手类
  *
- * @author huangyong
+ * @author pht
  * @since 1.0.0
  */
 public final class IocHelper {
@@ -25,7 +25,7 @@ public final class IocHelper {
             for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()) {
                 //获取BEAN类
                 Class<?> beanClass = beanEntry.getKey();
-                //获取BEAN实例
+                //获取BEAN实例，比如为controller类
                 Object beanInstance = beanEntry.getValue();
                 //获取BEAN类定义的所有成员变量
                 Field[] beanFields = beanClass.getDeclaredFields();
@@ -35,10 +35,11 @@ public final class IocHelper {
                         if (beanField.isAnnotationPresent(Inject.class)) {
                             //获取这个需要依赖注入的类名
                             Class<?> beanFieldClass = beanField.getType();
-                            //通过类名获得BEAN实例对象并注入给成员变量
+                            //通过类名获得BEAN实例对象
                             Object beanFieldInstance = beanMap.get(beanFieldClass);
                             if (beanFieldInstance != null) {
-                                //或者就通过反射初始化BEANFILED的值
+                                //如果该对象不为空证明是在map中，就将获取到的这个实例赋值给此时该类中需要注入的成员变量
+                                //比如，这里的beanInstance为controller，beanFieldInstance是service类
                                 ReflectionUtil.setField(beanInstance, beanField, beanFieldInstance);
                             }
                         }
