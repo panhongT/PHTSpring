049a21927c569bb5d28187be2d49d10fb911dacd
Hamtin <panht999@163.com>
2022-11-09 12:12:59 +0800

    MD5 密码匹配器

--- PhtSpring-plugin-security/src/main/java/com/pht/security/password/Md5CredentialsMatcher.java
+++ PhtSpring-plugin-security/src/main/java/com/pht/security/password/Md5CredentialsMatcher.java
@@ -1,15 +1,16 @@
 package com.pht.security.password;
 
+import com.pht.framework.util.CodecUtil;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
-import org.smart4j.framework.util.CodecUtil;
+
 
 /**
  * MD5 密码匹配器
  *
- * @author huangyong
+ * @author pht
  * @since 1.0.0
  */
 public class Md5CredentialsMatcher implements CredentialsMatcher {
